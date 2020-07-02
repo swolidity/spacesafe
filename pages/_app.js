a41b4240302fc3742d18faf163b939c7239d5172
Andy Kay <andykay.me@gmail.com>
2020-07-02 13:47:08 -0400

    initial chakra-ui setup

---
+++ pages/_app.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import {
+  ColorModeProvider,
+  ThemeProvider,
+  GlobalStyle,
+} from "@chakra-ui/system";
+import { CSSReset } from "@chakra-ui/core";
+import theme from "@chakra-ui/theme";
+
+export default ({ Component, pageProps }) => (
+  <ThemeProvider theme={theme}>
+    <ColorModeProvider>
+      <CSSReset />
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ColorModeProvider>
+  </ThemeProvider>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,41 @@
 import React from "react";
 import { useSession } from "next-auth/client";
 import CheckInForm from "../components/CheckInForm";
+import { Flex, Box, Avatar, Link } from "@chakra-ui/core";
 
 export default ({ data }) => {
   const [session, loading] = useSession();
 
   return (
     <div>
-      {!session && (
-        <>
-          Not signed in <br />
-          <a href="/api/auth/signin">Sign in</a>
-        </>
-      )}
-      {session && (
-        <>
-          <img
-            src={session.user.image}
-            alt={session.user.name}
-            height="80px"
-            style={{ borderRadius: "50%" }}
-          />
-          Signed in as {session.user.email} <br />
-          <a href="/api/auth/signout">Sign out</a>
-          <CheckInForm />
-        </>
-      )}
+      <Flex justify="space-between" align="center" px={6} py={3}>
+        SafeSpace
+        {!session && (
+          <Box>
+            <a href="/api/auth/signin">Sign in</a>
+          </Box>
+        )}
+        {session && (
+          <Box>
+            <Flex align="center">
+              <Avatar src={session.user.image} alt={session.user.name} mr={3} />
+
+              <Box>
+                <a href="/api/auth/signout">Sign out</a>
+              </Box>
+            </Flex>
+          </Box>
+        )}
+      </Flex>
+
+      <Box p={6} background="#f8f8f8" height="100%">
+        {!session && (
+          <Box>
+            <a href="/api/auth/signin">Sign in</a>
+          </Box>
+        )}
+        {session && <CheckInForm />}
+      </Box>
     </div>
   );
 };
