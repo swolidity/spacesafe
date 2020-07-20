6a7ec05da66755b1e1c601bea8c316ad90d3ba6f
Andy Kay <andykay.me@gmail.com>
2020-07-20 11:14:50 -0400

    add tabs

--- components/CheckInForm.js
+++ components/CheckInForm.js
@@ -1,12 +1,25 @@
 import { FielderProvider, useForm } from "fielder";
 import CheckInFormContent from "../components/CheckInFormContent";
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/core";
 
 export default ({ locations }) => {
   const state = useForm();
 
   return (
     <FielderProvider value={state}>
-      <CheckInFormContent />
+      <Tabs>
+        <TabList>
+          <Tab>Building</Tab>
+          <Tab>Field Site</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <CheckInFormContent />
+          </TabPanel>
+          <TabPanel>Field Sites</TabPanel>
+        </TabPanels>
+      </Tabs>
     </FielderProvider>
   );
 };
