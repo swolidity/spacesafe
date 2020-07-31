676d912a1c3e6173ff2d1ae7fb0e2a37d4bc4da9
Andy Kay <andykay.me@gmail.com>
2020-07-31 12:18:59 -0400

    lock down admin activity

--- pages/api/admin/activity.js
+++ pages/api/admin/activity.js
@@ -7,6 +7,14 @@ export default async function (req, res) {
   const session = await getSession({ req });
 
   if (req.method === "GET" && session) {
+    const user = await prisma.user.findOne({
+      where: {
+        email: session.user.email,
+      },
+    });
+
+    if (!user.isAdmin) throw new Error("Not authorized.");
+
     const logs = await prisma.activityLog.findMany({
       include: {
         location: true,
