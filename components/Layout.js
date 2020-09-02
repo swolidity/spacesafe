0dc7a79c3cdabcb48d01a5455420d7cd9514e9d5
Andy Kay <andykay.me@gmail.com>
2020-09-02 13:05:38 -0400

    add google analytics

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,26 @@
 import Header from "./Header";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <div>
+      <Head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=UA-2343150-50"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-2343150-50');
+        `,
+          }}
+        />
+      </Head>
       <Header />
       <div>{children}</div>
 
