f9c25d55f014b03e00ee90517d211a122f9e3a88
Andy Kay <andykay.me@gmail.com>
2020-12-07 15:28:56 -0500

    add info alert box

--- components/CheckInTabs.js
+++ components/CheckInTabs.js
@@ -9,8 +9,47 @@ import {
 } from "@chakra-ui/core";
 import CheckInForm from "../components/CheckInForm";
 import FieldSiteForm from "../components/FieldSiteForm";
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+  CloseButton,
+} from "@chakra-ui/core";
+import { useState } from "react";
 
 export default function CheckInTabs() {
+  const [alertState, setAlertState] = useState("open");
+
+  const handleAlertClose = () => {
+    setAlertState("close");
+  };
+
+  let alertBlock;
+  if (alertState === "open") {
+    alertBlock = (
+      <Alert status="info" my={2}>
+        <AlertIcon />
+        <AlertTitle mr={2}>
+          The CDC recently updated its guidance for what constitutes a contact
+          for COVID tracing to include the cumulative amount of time interacting
+          with an infected individiual over the course of a given day (15
+          minutes total, not just 15 minutes at once), so we strongly encourage
+          you to document even short term contacts with other individuals you
+          might encounter repeatedly throughout your day.
+        </AlertTitle>
+        <CloseButton
+          onClick={handleAlertClose}
+          position="absolute"
+          right="8px"
+          top="8px"
+        />
+      </Alert>
+    );
+  } else {
+    alertBlock = null;
+  }
+
   return (
     <Box>
       <Tabs>
@@ -18,7 +57,7 @@ export default function CheckInTabs() {
           <Tab>UM/UMM Building</Tab>
           <Tab>Off Campus Site</Tab>
         </TabList>
-
+        {alertBlock}
         <TabPanels>
           <TabPanel>
             <CheckInForm />
